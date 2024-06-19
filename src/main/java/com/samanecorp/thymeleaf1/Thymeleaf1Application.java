998f827b285438958a461d64aab2bcd9c800af1d
ngorseck <ngorsecka@gmail.com>
2024-06-19 16:55:36 +0200

    add queries with critaria API

--- src/main/java/com/samanecorp/thymeleaf1/Thymeleaf1Application.java
+++ src/main/java/com/samanecorp/thymeleaf1/Thymeleaf1Application.java
@@ -1,6 +1,9 @@
 package com.samanecorp.thymeleaf1;
 
+import com.samanecorp.thymeleaf1.dao.UserDao;
 import com.samanecorp.thymeleaf1.dto.UserDto;
+import com.samanecorp.thymeleaf1.entity.UserEntity;
+import com.samanecorp.thymeleaf1.exception.EntityNotFoundException;
 import com.samanecorp.thymeleaf1.service.IUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Thymeleaf1Application implements CommandLineRunner {
 
 	private IUserService userService;
+	private UserDao userDao;
 	public static void main(String[] args) {
 		SpringApplication.run(Thymeleaf1Application.class, args);
 	}
@@ -19,5 +23,37 @@ public class Thymeleaf1Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		userService.save(new UserDto(1L, "Ngor","SECK","seck@samanecorporation.com","passer"));
+		userService.save(new UserDto(2L, "Abdou","SENE","sene@samanecorporation.com","passer"));
+		userService.save(new UserDto(3L, "Oumar","DIOUF","diouf@samanecorporation.com","passer"));
+		userService.save(new UserDto(4L, "Moussa","DIATTA","diatta@samanecorporation.com","passer"));
+
+		System.out.println("=========================LOGIN================================");
+		userDao.login("seck@samanecorporation.com", "passer")
+				.map(user -> {
+					System.out.println(user.getEmail());
+					return user;
+				})
+				.orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+		System.out.println("=========================ORDER BY================================");
+		userDao.allUserOrderByLastName()
+				.map(userEntities -> {
+					System.out.println("User: " + userEntities.size() + " The first : " + userEntities.get(0).getLastName());
+					return userEntities.get(0);
+				}).orElseThrow(() -> new EntityNotFoundException("no user found"));
+
+		System.out.println("==========================COUNT===============================");
+		userDao.countAllUsers()
+				.map(number -> {
+					System.out.println("User count: " + number);
+					return number;
+				}).orElseThrow();
+
+		System.out.println("==========================Projections===============================");
+		userDao.allLastNameAndFirstName()
+				.map(data -> {
+					data.forEach(user -> System.out.println("FistName : " + user.get(0) + ", LastName : " + user.get(1)));
+					return data;
+				}).orElseThrow();
 	}
 }
--- src/main/java/com/samanecorp/thymeleaf1/dao/UserDao.java
+++ src/main/java/com/samanecorp/thymeleaf1/dao/UserDao.java
@@ -2,6 +2,8 @@ package com.samanecorp.thymeleaf1.dao;
 
 import com.samanecorp.thymeleaf1.entity.UserEntity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Tuple;
+import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -10,9 +12,11 @@ import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-
+@Repository
+@AllArgsConstructor
 public class UserDao {
 
 	EntityManager em;
@@ -31,5 +35,41 @@ public class UserDao {
 		cr.where(finalPredicate);
 		
 		return Optional.ofNullable(em.createQuery(cr).getSingleResult());
+	}
+
+	public Optional<List<UserEntity>> allUserOrderByLastName() {
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<UserEntity> cr = cb.createQuery(UserEntity.class);
+		Root<UserEntity> user = cr.from(UserEntity.class);
+
+		cr.orderBy(cb.asc(user.get("lastName")));
+		cr.select(user);
+
+		return Optional.ofNullable(em.createQuery(cr).getResultList());
+	}
+
+	public Optional<Long> countAllUsers() {
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> cr = cb.createQuery(Long.class);
+		Root<UserEntity> user = cr.from(UserEntity.class);
+
+		cr.select(cb.count(user));
+
+		return Optional.ofNullable(em.createQuery(cr).getSingleResult());
+	}
+
+	public Optional<List<Tuple>> allLastNameAndFirstName() {
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Tuple> cr = cb.createTupleQuery();
+		Root<UserEntity> user = cr.from(UserEntity.class);
+
+		cr.distinct(true);
+		cr.orderBy(cb.asc(user.get("lastName")));
+		cr.multiselect(user.get("firstName"), user.get("lastName"));
+
+		return Optional.ofNullable(em.createQuery(cr).getResultList());
 	}
 }
