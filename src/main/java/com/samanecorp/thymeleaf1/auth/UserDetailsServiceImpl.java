9971b739f721558a55fd2994093f96871dad929d
ngorseck <ngorsecka@gmail.com>
2024-06-24 17:08:37 +0200

    cicd with github action and sonar analysis

--- src/main/java/com/samanecorp/thymeleaf1/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/samanecorp/thymeleaf1/auth/UserDetailsServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
